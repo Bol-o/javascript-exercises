07b35bf4a7e8a392ba581070d0bdfc44c0bd86c2
Bolo <121732191+Bol-o@users.noreply.github.com>
2024-01-08 20:18:16 +0000

    missions accomplished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,13 +1,20 @@
-const repeatString = function(string, num) {
-    let repeatWord = ''
-    if (num < 0){
-        return 'ERROR'
+//let text = "How are you doing today?";
+//const myArray = text.split("");
+//let newText = [];
+let newerText = '';
+function reversing(text){
+    for (let i = text.length-1; i > -1; i--){
+        //newText.push(text[i]) 
+        newerText += text[i]
+         
     }
-    for (let i = 0; i < num; i++){
-        repeatWord += string
-    }
-    return repeatWord
-};
+    
+}
 
-// Do not edit below this line
-module.exports = repeatString;
+reversing("How are you doing today?")
+
+// console.log(myArray)
+// console.log(newText)
+// console.log(myArray.length)
+//console.log(newText.toString())
+console.log(newerText)
