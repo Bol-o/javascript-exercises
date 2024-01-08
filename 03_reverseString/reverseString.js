8cc55a91bc58411b260aad87ab4bdfe63dbebcf1
Bolo <121732191+Bol-o@users.noreply.github.com>
2024-01-08 20:14:33 +0000

    Easier way than splitting the strings
    Used only a 'reverse' loop and jsut added individual letters to a new
    string, dunno if this is correct, but it worked

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
 
+
+const reverseString = function(text) {
+    let newerText = ''; // moved this from outside the function and then last 4 tests worked
+    for (let i = text.length-1; i > -1; i--){
+        //newText.push(text[i]) 
+        
+        newerText += text[i]
+         
+    }
+return newerText
 };
+//console.log(reverseString('123! abc!'))
 
 // Do not edit below this line
 module.exports = reverseString;
