90438fa82064f1bff0be4b05604a47dff16d694f
Bolo <121732191+Bol-o@users.noreply.github.com>
2024-01-08 20:21:24 +0000

    added little comment inside the file
    Inside the file cause it shows what went inside from outside
    the function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,10 @@
 
 
 const reverseString = function(text) {
-    let newerText = ''; // moved this from outside the function and then last 4 tests worked
+    let newerText = ''; // moved this from outside the function and then last 4 tests worked    
     for (let i = text.length-1; i > -1; i--){
         //newText.push(text[i]) 
-        
+            
         newerText += text[i]
          
     }
