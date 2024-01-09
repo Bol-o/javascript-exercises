52f29b710fc8f6364c8371400c63336ebb46c40e
Bolo <121732191+Bol-o@users.noreply.github.com>
2024-01-09 10:02:54 +0000

    Trying out const declaration of variables
    Declaration of a const variable allows you to make it's content changed,
    'new' that from reading, just testing in practise, again

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,16 +1,17 @@
-
+const text = '123 count'
 
 const reverseString = function(text) {
     let newerText = ''; // moved this from outside the function and then last 4 tests worked    
     for (let i = text.length-1; i > -1; i--){
         //newText.push(text[i]) 
-            
+
         newerText += text[i]
          
     }
-return newerText
+    text = newerText
+return text
 };
-//console.log(reverseString('123! abc!'))
+console.log(reverseString(text))
 
 // Do not edit below this line
 module.exports = reverseString;
