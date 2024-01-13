9a258694c08f85a1bc8217880accb9c10b391df4
Bolo <121732191+Bol-o@users.noreply.github.com>
2024-01-13 18:03:03 +0000

    all tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,26 @@
 
 
 
-const sumAll = function(first, last) {
+const sumAll = function(theFirst, theLast) {
+    let numbzo = 1
+    let numbzoType = typeof numbzo //couldn't compare with 'number', as js doesn't have number as an 'object', so this way worked
+    let first
+    let last 
+    if (theFirst > theLast){
+        first = theLast
+        last = theFirst
+    }else if(theFirst < theLast){
+        first = theFirst
+        last = theLast
+    }
+    
+
+    if (typeof first !== numbzoType || typeof last !== numbzoType || first < 0 || last < 0){
+        return 'ERROR'
+    }
     let array = []
     let theSum = 0
+    
     if (first < last){
        array.push(first)
        
@@ -30,6 +47,6 @@ const sumAll = function(first, last) {
              return array
 
 };
-console.log(sumAll(1,6))
+console.log(sumAll(5, 1))
 // Do not edit below this line
 module.exports = sumAll;
