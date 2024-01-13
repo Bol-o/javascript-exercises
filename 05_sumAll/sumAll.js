9d27748b4adfc490e8390f89f013f579cc789e05
Bolo <121732191+Bol-o@users.noreply.github.com>
2024-01-13 14:06:32 +0000

    first test done, working on the rest

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,35 @@
-const sumAll = function() {
+
+
+
+const sumAll = function(first, last) {
+    let array = []
+    let theSum = 0
+    if (first < last){
+       array.push(first)
+       
+      }  
+         for (let i = 0; i < array.length; i++){
+            if (array[i] == last){
+            
+                
+            }
+            if (array[i] < last)
+                
+                array.push(array[i]+1)
+                 
+            
+             }
+    
+             for (let i = 0; i < array.length; i++){
+                theSum = theSum + array[i]
+                
+             }
+             
+        
+             return theSum
+             return array
 
 };
-
+console.log(sumAll(1,6))
 // Do not edit below this line
 module.exports = sumAll;
