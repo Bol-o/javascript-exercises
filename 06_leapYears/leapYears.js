f2224343e8232d130b4eb27aafa4c272e411c49b
Bolo <121732191+Bol-o@users.noreply.github.com>
2024-01-14 20:28:21 +0000

    used numberIsInteger, quite glad I figured it out

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+if(Number.isInteger((year)/400) === true ){
+    return true 
+}else if (Number.isInteger((year)/4) === true && Number.isInteger((year)/100) === false){
+    return true
+}
+else {
+    return false
+}
+
 
 };
-
+console.log(leapYears(34992))
 // Do not edit below this line
 module.exports = leapYears;
