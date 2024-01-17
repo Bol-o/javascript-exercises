c6fbb07cda28f5bd5e2ec4c5d64971df0fdfcee4
Bolo <121732191+Bol-o@users.noreply.github.com>
2024-01-17 11:18:34 +0000

    .toFixed() changes into string
    Didn't know that it rounded up to one digit after coma, AND converts
    into strings, even if entering numbers, but it was so easy just to
    convert into numbers. After trying 2 figure out what was goin wrong hehe
    hence planty of cons.log with typeof

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,26 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInF) {
+  let tempInFar = tempInF
+  console.log(typeof tempInFar)
+  let result =  (tempInFar - 32) * 5/9;
+  console.log(typeof result)
+  result = Number(result.toFixed(1))
+  // if (result == 0){
+  //   return 0 
+  // }
+  console.log(typeof result)
+  return result
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+
+  //let tempInCel = tempInC
+  //console.log(typeof result)
+  //let result 
+  let result =  (tempInC* 9/5) + 32 ;
+  result = Number(result.toFixed(1))
+  console.log(typeof result)
+  return result
+
 };
 
 // Do not edit below this line
@@ -9,3 +28,7 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+console.log(convertToCelsius(32))
+console.log(convertToFahrenheit(0))
+console.log(7)
